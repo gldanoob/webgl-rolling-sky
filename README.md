6a2b8d0d9667cdc3e16c56126113881191eabcce
gldanoob <caleblolhk@gmail.com>
2023-03-27 02:41:33 +0100

    formatting + readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+<https://rolling-sky.jser.repl.co>
 Originally hosted at: <https://webgl-rolling-sky--iamcaleblol.repl.co/>
--- controls.js
+++ controls.js
@@ -1,18 +1,7 @@
 canvas.on('mousemove', e => {
-  e.preventDefault();
-  if (started) {
-		const pos = (e.clientX - $(window).width() / 2) / (($(window).width() < 450? $(window).width(): 450) / 5);
-		if (pos >= -3.5 && pos <= 3.5) {
-			ball.mesh.position.x = pos;
-			camera.position.x = pos / 7;
-		}
-  }
-});
-canvas.on('touchmove', e => {
-  e.preventDefault();
+	e.preventDefault();
 	if (started) {
-		const pos =
-			(e.changedTouches[0].pageX - $(window).width() / 2) / (($(window).width() < 450? $(window).width(): 450) / 5);
+		const pos = (e.clientX - $(window).width() / 2) / (($(window).width() < 450 ? $(window).width() : 450) / 5);
 		if (pos >= -3.5 && pos <= 3.5) {
 			ball.mesh.position.x = pos;
 			camera.position.x = pos / 7;
@@ -20,17 +9,28 @@ canvas.on('touchmove', e => {
 	}
 });
 
+canvas.on('touchmove', e => {
+	e.preventDefault();
+	if (started) {
+		const pos =
+			(e.changedTouches[0].pageX - $(window).width() / 2) / (($(window).width() < 450 ? $(window).width() : 450) / 5);
+		if (pos >= -3.5 && pos <= 3.5) {
+			ball.mesh.position.x = pos;
+			camera.position.x = pos / 7;
+		}
+	}
+});
 
 $(window).on('keydown', e => {
-  if(started && !ball.count2Lose) keystate[e.keyCode] = true;
+	if (started && !ball.count2Lose) keystate[e.keyCode] = true;
 });
 
 $(window).on('keyup', e => {
-  delete keystate[e.keyCode];
+	delete keystate[e.keyCode];
 });
 
 $(window).on('resize', e => {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 });
